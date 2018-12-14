7a1251e11168d121464665b38b74fc6819db7d06
Cian Durrigan <cian@adaptis.io>
2018-12-14 10:58:33 +0700

    add multiple props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog post!"
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="Interesting blog post"
+        avatar={Faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I totally agree!!"
+        avatar={Faker.image.avatar()} 
+      />
     </div>
   );
 }
